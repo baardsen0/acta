4e7add55363f214ee9217c0d70dd418805732919
Baardsen <freddy@baardsen.io>
2026-02-26 00:24:27 +0100

    fix for issue with docker

--- main.py
+++ main.py
@@ -19,5 +19,5 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    logger.info("Starting acta web server on http://127.0.0.1:5003")
-    app.run(host="127.0.0.1", port=5003, debug=True)
+    logger.info("Starting acta web server on http://0.0.0.0:5003")
+    app.run(host="0.0.0.0", port=5003)
